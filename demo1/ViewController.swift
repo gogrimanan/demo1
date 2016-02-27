720f42b8ef83d61967f76a064c441dc4658261f0
gogrimanan <manan@manans-macbook-pro.local>
2016-02-27 17:51:01 -0500

    Initial Commit

---
+++ demo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo1
+//
+//  Created by Manan Gogri on 2/27/16.
+//  Copyright © 2016 Manan Gogri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
